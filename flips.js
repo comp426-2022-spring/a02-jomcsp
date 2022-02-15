ead08ef9950d42bfcc3bb2cc22bc074fa14917d8
jomcsp <jomcsp@live.unc.edu>
2022-02-15 14:26:55 -0500

    added edge cases for errors

--- flips.js
+++ flips.js
@@ -1,12 +1,12 @@
-import { coinFlips, countFlips } from "./modules/coin.mjs";
+import { coinFlip, coinFlips, countFlips } from "./modules/coin.mjs";
 import minimist from 'minimist'
 const args = (process.argv.slice(2));
 let num = 0
 if (args.length > 0) {
     num = args[0].slice(9,(args[0].length));
+    const answer = coinFlips(num);
+    console.log(answer);
+    console.log(countFlips(answer));
 } else {
-    num = 1;
+    console.log(coinFlip());
 }
-const answer = coinFlips(num);
-console.log(answer);
-console.log(countFlips(answer));
--- guess-flip.js
+++ guess-flip.js
@@ -1,14 +1,21 @@
 import { flipACoin } from "./modules/coin.mjs";
 import minimist from 'minimist'
 const args = (process.argv.slice(2));
-let call= args[0].slice(7,(args[0].length));
-
-
-if (call === 'heads' || call === 'tails') {
-    console.log(flipACoin(call));
-} else {
-    console.log("Error: no or wrong input");
-    console.log("Usage: node guess-flip --call=[heads|tails]"); 
+try {
+    let call= args[0].slice(7,(args[0].length));
+    if (call === 'heads' || call === 'tails') {
+        console.log(flipACoin(call));
+    } else {
+        console.log("Error: no or wrong input");
+        console.log("Usage: node guess-flip --call=[heads|tails]"); 
+    }
 }
+catch (e) {
+    console.log("Error: no or wrong input");
+    console.log("Usage: node guess-flip --call=[heads|tails]");
+}
+
+
+
 
 
